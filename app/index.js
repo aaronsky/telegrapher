9b065ddce9ec518054fb7882e8190f5ed0d33260
Ben Wilcox <reccanti@gmail.com>
2016-05-09 09:18:10 -0400

    Views
    Created an index view for the app

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 
 module.exports = function (type, options) {
     options = options || {};
@@ -11,14 +12,24 @@ module.exports = function (type, options) {
     if (!model) {
         throw new Error('An invalid model type was passed. You must pass either \'client\' or \'server\' as a parameter.');
     }
-    
+
     var app = express();
+
+    if (type === 'server') {
+        app.set(express.static(path.join(__dirname, 'server', 'public')));
+        app.set('views', path.join(__dirname, 'server', 'views'));
+        app.set('view engine', 'pug');
         
+        app.get('/', function (req, res) {
+            res.render('index');
+        });
+    }
+    
     // error handlers
     // development error handler
     // will print stacktrace
     if (app.get('env') === 'development') {
-        app.use(function(err, req, res, next) {
+        app.use(function (err, req, res, next) {
             res.status(err.status || 500);
             res.render('error', {
                 message: err.message,
@@ -29,15 +40,16 @@ module.exports = function (type, options) {
 
     // production error handler
     // no stacktraces leaked to user
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
             error: {}
         });
     });
-    
+
+
     app.io = model(options);
-    
+
     return app;
 };
