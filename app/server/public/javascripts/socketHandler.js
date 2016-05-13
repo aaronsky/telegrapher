7d966af7ebebe3ac080a1d99c226d278a86950e0
Ben Wilcox <reccanti@gmail.com>
2016-05-13 11:35:03 -0400

    jQuery Message Object
    Created a messageBox object that uses jQuery to creqte an element that holds a message. This object has methods for attaching itself to an element and writing to the existing message object. Coupled with this are minor changes to CSS to make the messages more readable

--- app/server/public/javascripts/socketHandler.js
+++ app/server/public/javascripts/socketHandler.js
@@ -3,6 +3,9 @@ var Note = {
     SHORT: 1,
     LONG: 2
 };
+var curSpeaker = '';
+var activeMessage = messageBox('');
+
 
 window.addEventListener('load', function () {
     var list = $('#message_container');
@@ -36,8 +39,48 @@ function listenForInput(socket, list) {
         } else if (data.note === Note.OFF) {
             str = ' ';
         }
-        list.prepend(str);
-        console.log('I have listened');
-        console.log(data);
+        
+        if (data.name !== curSpeaker) {
+            curSpeaker = data.name;
+            activeMessage = messageBox(curSpeaker);
+            activeMessage.attach(list);
+        };
+        activeMessage.write(str);
     });
+};
+
+
+/**
+ * A container for a speakerBox containing a message
+ */
+function messageBox(speakerName) {
+    var $container = $('<div></div>', {class:'message'});
+    var $speaker = $('<span></span>', {class: 'message_speaker'});
+    var $text = $('<span></span>', {class:'message_text'});
+    
+    $speaker.text(speakerName + ': ');   
+    $container.prepend($text);
+    $container.prepend($speaker);
+    
+    /**
+     * Prepend the messageBox to the specified
+     * element
+     */
+    var attach = function ($el) {
+        $el.prepend($container);
+    };
+    
+    
+    /**
+     * given a character, write it in the text container
+     * of the messageBox.
+     */
+    var write = function (char) {
+        $text.prepend(char);
+    };
+    
+    return {
+        attach: attach,
+        write: write
+    }
 };
